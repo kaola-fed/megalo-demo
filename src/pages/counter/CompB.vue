45e693c5e16968ce8f127639f2a131fbd0b50f1a
elcarim5efil <elcarim5efil@gmail.com>
2018-08-17 15:33:40 +0800

    chore: update

---
+++ src/pages/counter/CompB.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="b">
+   {{ message }} 
+   <div>
+    <slot></slot>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: 'CompB'
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/pages/counter/CompC.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="c" @click="onClick($event)">
+   {{ message }} 
+   <div>
+    <slot></slot>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: 'CompC'
+    }
+  },
+  methods: {
+    onClick(e) {
+      this.$emit('click', {
+        sender: this,
+        comp: 'C',
+        e
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
