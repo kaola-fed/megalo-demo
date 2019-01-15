29f140522c278432b36cb0f9418c49e5b96a7ddc
bigMeow <running@vip.163.com>
2019-01-15 15:06:24 +0800

    feature: 配置插件化

--- build/util.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const contextDir = path.resolve(process.cwd(), '.')
-function resolve (...args) {
-  return path.resolve(contextDir, ...args)
-}
-
-exports.contextDir = contextDir
-exports.resolve = resolve
