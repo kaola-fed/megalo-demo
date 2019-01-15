6f42e6d9830c450bbb02a362f69e871245943de4
bigMeow <running@vip.163.com>
2019-01-15 15:22:14 +0800

    add options desc

--- megalo.config.js
+++ megalo.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  // 生成分析报告
-  bundleAnalyzerReport: false,
+  // 构件生产模式时的配置（仅在process.env.NODE_ENV === 'production' 时该选项生效）
+  build: {
+    // 生成分析报告
+    bundleAnalyzerReport: false,
+    // 生成sourceMap
+    sourceMap: false
+  },
   configureWebpack: config => {
     // 你可以在这里修改webpack的配置并返回
     return config
